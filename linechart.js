687dc61abb6804ad2d2ac7b405e7ab2a9b524c6f
calcuis <113646141+calcuis@users.noreply.github.com>
2022-11-16 09:57:04 +0800

    Add files via upload

---
+++ linechart.js
@@ -0,0 +1,16 @@
+class Line extends react.exports.Component {
+  render() {
+    return /* @__PURE__ */ jsx(Plot, {
+      data: [{
+        x: ["Jul", "Aug", "Sep", "Oct", "Nov"],
+        y: [250, 200, 300, 450, 500],
+        type: "scatter"
+      }],
+      layout: {
+        width: 800,
+        height: 600,
+        title: "Monthly revenue"
+      }
+    });
+  }
+}
